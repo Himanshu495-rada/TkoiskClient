e239db4a7ee6e7d62e3656e1d2a3ec515a1d6279
Himanshu Tekade <himanshu.10711770@ltimindtree.com>
2024-03-18 13:19:31 +0530

    updated messagesSection

--- src/Components/LoginSignupSection/Login/Login.jsx
+++ src/Components/LoginSignupSection/Login/Login.jsx
@@ -1,107 +1,138 @@
-import { useState } from "react"
-import { useRecoilState } from 'recoil'
-import "./Login.css"
-import symbol from "../../../Assets/symbol-bgr.png"
+import { useState } from "react";
+import { useRecoilState } from "recoil";
+import styles from "./Login.module.css";
+import symbol from "../../../Assets/symbol-bgr.png";
 import { CurrentUser, loginUser } from "../../../Services/UserService";
-import { loginState } from '../../../State/atoms/loginState'
-import { Link, useNavigate } from 'react-router-dom'
-import { useQuery, useMutation, gql } from '@apollo/client'
-import client from '../../../apolloClient';
+import { loginState } from "../../../State/atoms/loginState";
+import { Link, useNavigate } from "react-router-dom";
+import { useQuery, useMutation, gql } from "@apollo/client";
+import client from "../../../apolloClient";
 
 function Login() {
   const [credentials, setcredentials] = useState({ email: "", password: "" });
-  const [isLoggedIn, setisLoggedIn] = useRecoilState(loginState)
-  const [isLoading, setisLoading] = useState(false)
-  const [errMsg, seterrMsg] = useState(null)
+  const [isLoggedIn, setisLoggedIn] = useRecoilState(loginState);
+  const [isLoading, setisLoading] = useState(false);
+  const [errMsg, seterrMsg] = useState(null);
   const navigate = useNavigate();
 
   const LOGIN = gql`
-  mutation Login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
+    mutation Login($email: String!, $password: String!) {
+      login(email: $email, password: $password) {
+        token
+      }
     }
-  }`;
+  `;
 
   const ME = gql`
-  query Query {
-    me {
-      username
-      name
-      _id
+    query Query {
+      me {
+        username
+        name
+        _id
+      }
     }
-  }`;
+  `;
 
   const [login] = useMutation(LOGIN, {
     onCompleted: async (data) => {
       setisLoading(false);
-      localStorage.setItem('authToken', data.login.token);
-      const { loading, error, data: userData } = await client.query({
+      localStorage.setItem("authToken", data.login.token);
+      const {
+        loading,
+        error,
+        data: userData,
+      } = await client.query({
         query: ME,
       });
       if (userData) {
         console.log(userData);
-        localStorage.setItem('username', userData.me.username);
-        localStorage.setItem('name', userData.me.name);
-        localStorage.setItem('user_id', userData.me._id);
+        localStorage.setItem("username", userData.me.username);
+        localStorage.setItem("name", userData.me.name);
+        localStorage.setItem("user_id", userData.me._id);
         setisLoggedIn(true);
-        navigate("/main/home")
+        navigate("/main/home");
         //window.location.reload();
-      }
-      else {
-        seterrMsg("something went wrong!")
+      } else {
+        seterrMsg("something went wrong!");
       }
     },
     onError: (error) => {
       console.log(error);
-    }
+    },
   });
 
   const handleSubmit = async (e) => {
-    setisLoading(true)
+    setisLoading(true);
     e.preventDefault();
-    await login({ variables: { email: credentials.email, password: credentials.password } });
-  }
-  
+    await login({
+      variables: { email: credentials.email, password: credentials.password },
+    });
+  };
+
   const onChange = (e) => {
-    seterrMsg(null)
-    setisLoading(false)
+    seterrMsg(null);
+    setisLoading(false);
     setcredentials({ ...credentials, [e.target.name]: e.target.value });
   };
   return (
-    <div className="App">
-      <div className="login-container">
-        <div className="floating-icon">
+    <div className={styles.App}>
+      <div className={styles.login_container}>
+        <div className={styles.floating_icon}>
           <img src={symbol} alt="" />
         </div>
         <h1>Login</h1>
         <form onSubmit={handleSubmit}>
-          {errMsg !== null ?
-            <div class="alert alert-danger d-flex align-items-center" role="alert">
-              <i class="bi bi-exclamation-triangle"></i>
-              <div>
-                {errMsg}
-              </div>
-            </div> :
-            errMsg}
+          {errMsg !== null ? (
+            <div
+              className={`alert alert-danger d-flex align-items-center`}
+              role="alert"
+            >
+              <i className={`bi bi-exclamation-triangle`}></i>
+              <div>{errMsg}</div>
+            </div>
+          ) : (
+            errMsg
+          )}
 
-          <input className="login-email" type="email" placeholder="Email" name="email" value={credentials.email} onChange={onChange} required />
-          <input className="login-password" type="password" placeholder="Password" name="password" value={credentials.password} onChange={onChange} required />
+          <input
+            className={styles.login_email}
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={credentials.email}
+            onChange={onChange}
+            required
+          />
+          <input
+            className={styles.login_password}
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={credentials.password}
+            onChange={onChange}
+            required
+          />
           <button type="submit">
-            {isLoading ?
-              <div className="spinner-border spinner-border-sm text-light" role="status">
+            {isLoading ? (
+              <div
+                className={`spinner-border spinner-border-sm text-light`}
+                role="status"
+              >
                 <span className="visually-hidden">Loading...</span>
               </div>
-              : "Log In"
-            }</button>
+            ) : (
+              "Log In"
+            )}
+          </button>
         </form>
-        <div className="extra-links">
-          <Link to={"/join/forgotpwd"} >Forgot Password</Link>
+        <div className={styles.extra_links}>
+          <Link to={"/join/forgotpwd"}>Forgot Password</Link>
           <span>|</span>
-          <Link to={"/join/signup"} >Register Here</Link>
+          <Link to={"/join/signup"}>Register Here</Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/Components/MessagesSection/MessagesSection.jsx
+++ src/Components/MessagesSection/MessagesSection.jsx
@@ -1,17 +1,72 @@
 import React from "react";
 import styles from "./MessagesSection.module.css";
+import person_1 from "../../Assets/person_1.jpg";
+import person_2 from "../../Assets/person_2.jpg";
+import giorgia_meloni from "../../Assets/Giorgia_Meloni.jpg";
+import narendra_modi from "../../Assets/Narendra_Modi.png";
 
 function MessagesSection() {
   return (
     <div className={styles.messages_section}>
       <section className={styles.header}>
         <h1 className={styles.title}>Messages</h1>
-        <i className="bi bi-gear"></i>
-        <i class="bi bi-envelope-plus"></i>
+        <div>
+          <i className="bi bi-gear" style={{ marginRight: "10px" }}></i>
+          <i class="bi bi-envelope-plus"></i>
+        </div>
       </section>
-      <section className={styles.active_profiles}></section>
-      <section className={styles.search_section}></section>
-      <section className={styles.messages_section}></section>
+
+      <section className={styles.section_2}>
+        <p className={styles.section_2_title}>Active profiles</p>
+        <div className={styles.profiles_container}>
+          <img
+            className={styles.active_profile_img}
+            src={person_1}
+            alt="person_1"
+          />
+          <img
+            className={styles.active_profile_img}
+            src={person_2}
+            alt="person_2"
+          />
+        </div>
+      </section>
+
+      <section className={styles.section_3}>
+        <i className={`bi bi-search ${styles.section_3_icon}`}></i>
+        <input
+          type="text"
+          className={styles.section_3_input}
+          placeholder="Search Direct Messages"
+        />
+      </section>
+
+      <section className={styles.section_4}>
+        <table className={styles.section_4_table}>
+          <tr className={styles.section_4_row}>
+            <img
+              className={styles.section_4_profile_img}
+              src={giorgia_meloni}
+              alt="Giorgia Meloni"
+            />
+            <div className={styles.section_4_profile_details}>
+              <h1 className={styles.section_4_profile_name}>Giogia Meloni</h1>
+              <p className={styles.section_4_profile_id}>@giorgia_meloni</p>
+            </div>
+          </tr>
+          <tr className={styles.section_4_row}>
+            <img
+              className={styles.section_4_profile_img}
+              src={narendra_modi}
+              alt="Narendra Modi"
+            />
+            <div className={styles.section_4_profile_details}>
+              <h1 className={styles.section_4_profile_name}>Narendra Modi</h1>
+              <p className={styles.section_4_profile_id}>@narendra_modi</p>
+            </div>
+          </tr>
+        </table>
+      </section>
     </div>
   );
 }
